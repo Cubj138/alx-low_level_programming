5e86544ab809baa97e52e34ddf3569375db4145e
Jacob Stephen <jacobstephen045@gmail.com>
2024-02-01 12:30:43 -0800

    Printing strings

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *Prints a string in C
+ *
+ *Return: 0(Success) 
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
