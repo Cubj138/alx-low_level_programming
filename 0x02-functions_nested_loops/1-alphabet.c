16bbe57ed3b7ea584c267dc797364643e8b440e4
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-15 01:42:12 -0700

    printing the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet im lowercase
+ *
+ * Return: 0(success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar('a');
+
+	_putchar(10);
+	return (0);
+}
