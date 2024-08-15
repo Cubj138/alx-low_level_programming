8cac6be0c0771c7505163938997d689472dba4b3
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-15 05:14:33 -0700

    Prints the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of an integer
+ *@c: parameter
+ *
+ * Return: returns absolute value
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
+	return (0);
+}
