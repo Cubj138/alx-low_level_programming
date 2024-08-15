6a1d0839adc168eb71eb650a61b2b5008befc6bf
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-15 01:47:26 -0700

    Pronting all the letters of the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet - prints the alphabet im lowercase
  *
- * Return: 0(success)
+ * Return: returns nothing
  */
 
 void print_alphabet(void)
@@ -11,8 +11,7 @@ void print_alphabet(void)
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
-		_putchar('a');
+		_putchar(i);
 
 	_putchar(10);
-	return (0);
 }
