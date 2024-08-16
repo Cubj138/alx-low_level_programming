fc3abe72e31212936191bb63da9c755c8c7d16fd
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-16 05:13:04 -0700

    Prints numbers from n up to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,26 +13,19 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (i = n; i < 98; i++)
 		{
-			printf("%d", i);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", i);
 		}
+		printf("98\n");
 	}
 	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d", i);
-			if (n != 98)
-			{
-				printf(", ");
-			}
+			printf("%d, ", i);
 		}
+		printf("98\n");
 	}
-	printf("\n");
 }
 
