3e20625208604f7b64b10121836b8fb210f38d9c
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-16 04:41:50 -0700

    Adds the two parameters together and returns answer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ *@a: parameter
+ *@b: parameter
+ *
+ * Return: returns addition result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
