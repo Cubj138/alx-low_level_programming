b78b5696ee38322d88a13fcca7a8eee086ff31c4
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-19 06:14:03 -0700

    Prints 0 to 14 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 10 times
+ *
+ * Return: returns nothing
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			if (j > 9)
+			{
+			_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
