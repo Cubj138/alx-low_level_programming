709ffad6f434c7028b5b69f5b83e196e9efb92c6
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-19 07:02:54 -0700

    Prints numbers form 1 to 100 whle replacing some

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - prints digits from 1 to 100 replacing multiples of 3, 5 and (3 and 5)
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (++i <= 100)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+
+			if (i != 100)
+			{
+				printf("Buzz ");
+			}
+			else
+				printf("Buzz");
+		}
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
