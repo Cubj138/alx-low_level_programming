70c2267af7bffbdb72fe1703d1cb138a9bb07bef
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-19 06:32:14 -0700

    prints a diagonal using n

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal according to n
+ *@n: parameter
+ *
+ * Return: returns nothing
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int j;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
