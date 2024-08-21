3dec89c6adb989058aa49a4b5dc6adef3e3825dc
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-21 06:36:12 -0700

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *@s: parameter
+ *
+ * Return: returns nothing
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i -= 1;
+	for (j = 0; j < i / 2; j++)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+		i--;
+	}
+}
