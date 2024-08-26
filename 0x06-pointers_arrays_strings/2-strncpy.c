e1cb3890a41694f905ed544de8b1d954b9fbb220
Jacob Stephen <jacobstephen045@gmail.com>
2024-08-26 05:51:58 -0700

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies one string to another
+ *@dest: parameter
+ *@src: parameter
+ *@n: parameter
+ *
+ * Return: returns dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
